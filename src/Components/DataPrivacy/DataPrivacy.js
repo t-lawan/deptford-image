44352ae701373d3b857e27ebfd18787a59dd6d5a
akinsola <thomas.lawanson@gmail.com>
2020-07-14 10:50:19 +0100

    update

--- src/Components/DataPrivacy/DataPrivacy.js
+++ src/Components/DataPrivacy/DataPrivacy.js
@@ -4,10 +4,25 @@ import styled from "styled-components";
 import { hasAgreedToTerms } from "../../Store/action";
 import { Colour } from "../Global/global.styles";
 import ReactGA from "react-ga";
-
+import { Link } from 'react-router-dom';
 
 const DataPrivacyWrapper = styled.div`
     background: ${Colour.green};
+    opacity: 0.6;
+    display: ${props => (props.show ? "flex" : "none")};
+    flex-direction: row;
+    padding: 0.5rem 1rem;
+    width: 100%;
+    height: 10vh;
+    z-index: 1200;
+    bottom: 0;
+    left: 0%;
+    position: fixed;
+
+`;
+
+const DataPrivacyText = styled.p`
+    display: inline-flex;
 `
 class DataPrivacy extends React.Component {
   componentDidMount() {
@@ -15,16 +30,16 @@ class DataPrivacy extends React.Component {
       if (window.sessionStorage.getItem("AGREED_TO_PRIVACY")) {
         if (!this.props.has_agreed_to_terms) {
           this.props.hasAgreedToTerms();
-          this.initializeReactGA()
+          this.initializeReactGA();
         }
       }
     }
   }
 
- initializeReactGA = () => {
+  initializeReactGA = () => {
     ReactGA.initialize("UA-153315241-2");
     ReactGA.pageview("/");
-  }
+  };
 
   setAgreedToTrue = () => {
     if (!this.props.has_agreed_to_terms) {
@@ -32,31 +47,38 @@ class DataPrivacy extends React.Component {
       if (typeof window !== `undefined`) {
         if (!window.sessionStorage.getItem("AGREED_TO_PRIVACY")) {
           window.sessionStorage.setItem("AGREED_TO_PRIVACY", true);
-          this.initializeReactGA()
+          this.initializeReactGA();
         }
       }
     }
   };
 
   render() {
-      <DataPrivacyWrapper>
-
+    return (
+      <DataPrivacyWrapper show={!this.props.has_agreed_to_terms && !this.props.show_instructions}>
+      <div>
+        <DataPrivacyText> By using this website you agree to the use of cookies in accordance with our <Link to={'/data-privacy'}>data privacy policy</Link>. </DataPrivacyText>
+      </div>
+        <DataPrivacyText> Okay</DataPrivacyText>
       </DataPrivacyWrapper>
+    );
   }
 }
 
 const mapStateToProps = state => {
-    return {
-      has_agreed_to_terms: state.agreed_to_terms,
-    }
-  }
-  
-  const mapDispatchToProps = dispatch => {
-    return {
-        hasAgreedToTerms: () => dispatch(hasAgreedToTerms()),
-    }
-  }
-  
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(DataPrivacy)
-  
+  return {
+    has_agreed_to_terms: state.agreed_to_terms,
+    show_instructions: state.show_instructions
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    hasAgreedToTerms: () => dispatch(hasAgreedToTerms())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DataPrivacy);
--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -6,6 +6,7 @@ import Instruction from "../Instruction/Instruction";
 import { GlobalStyle } from "../Global/global.styles";
 import SharingUrl from "../../Assets/seo_image.jpg";
 import ReactGA from "react-ga";
+import DataPrivacy from "../DataPrivacy/DataPrivacy";
 const Layout = props => {
   let description = "DEMO moving image festival is pleased to present its first online edition, taking place from the 15th of June to the 24th of July. As a response to the Covid-19 emergency, this edition of the festival will focus on the topic of Contagion.";
   let url = "https://demomovingimage.net/";
@@ -80,6 +81,7 @@ const Layout = props => {
       <GlobalStyle />
       <Loading />
       <Modal />
+      {/* <DataPrivacy /> */}
       <Instruction />
       <div>{props.children}</div>
     </>
